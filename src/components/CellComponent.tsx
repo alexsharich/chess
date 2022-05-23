23d63c3a48ce6eab8e61689a62f8f3b7963ffcd4
alexsharich <alexsharich@gmail.com>
2022-05-23 08:04:44 +0200

    add position for figure on board

--- src/components/CellComponent.tsx
+++ src/components/CellComponent.tsx
@@ -8,8 +8,8 @@ interface CellProps {
 
 export const CellComponent: FC<CellProps> = ({ cell }) => {
     return (
-        <div className={['cell',cell.color].join(' ')}>
-
+        <div className={['cell', cell.color].join(' ')}>
+            {cell.figure?.logo && <img src={cell.figure.logo} alt='' />}
         </div>
     )
 }
--- src/models/figures/Figure.ts
+++ src/models/figures/Figure.ts
@@ -1,3 +1,37 @@
+import { Colors } from "../Colors";
+import logo from '../../assets/black-king.png'
+import { Cell } from "../Cell";
+
+export enum FigureName {
+    FIGURE = 'Фигура',
+    KING = 'Король',
+    KNIGHT = 'Конь',
+    PAWN = 'Пешка',
+    QUEEN = 'Ферзь',
+    ROOK = 'Ладья',
+    BISHOP = 'Слон',
+}
+
 export class Figure {
-    
+    color: Colors;
+    logo: typeof logo | null;
+    cell: Cell;
+    name: FigureName;
+    id: number;
+
+    constructor(color: Colors, cell: Cell) {
+        this.color = color;
+        this.cell = cell;
+        this.cell.figure = this;
+        this.logo = null;
+        this.name = FigureName.FIGURE;
+        this.id = Math.random()
+    }
+
+    canMove(target: Cell) {
+
+    }
+    moveFigure(target:Cell){
+
+    }
 }
